e2f4a41418a4b5a88bdd720a9b80585d559660a0
Tianyao Chen <tillchen417@gmail.com>
2020-09-24 21:46:26 +0200

    Update 42MinimalTree.py

--- Chapter4/42MinimalTree.py
+++ Chapter4/42MinimalTree.py
@@ -16,11 +16,13 @@ def initiateArrayToBinary(array):
 def arrayToBinary(array, start, end):
     if start > end:
         return ''
-    mid = (start + end) / 2
+    mid = (start + end) // 2
     root = Node(array[mid])
     root.left = arrayToBinary(array, start, mid - 1)
     root.right = arrayToBinary(array, mid + 1, end)
     return root
 
-testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 18, 22, 43, 144, 515, 4123]
-print(initiateArrayToBinary(testArray))
+
+if __name__ == '__main__':
+    testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 18, 22, 43, 144, 515, 4123]
+    print(initiateArrayToBinary(testArray))
